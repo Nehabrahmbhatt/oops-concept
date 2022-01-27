cab923826a9d98ad5b986e9e2a7cc93326cbcfac
neha <nehabrahmbhatt5@yahoo.co.uk>
2022-01-27 17:27:51 +0000

    this is my first commit

---
+++ OopsConcept/Dog.java
@@ -0,0 +1,11 @@
+package OopsConcept;
+
+public class Dog extends Animals{
+    public void breeds(){
+        System.out.println("Animal ---breeds");
+    }
+    public void Run(){
+        System.out.println("Animal ---Run");
+
+    }
+}
---
+++ OopsConcept/RunTest.java
@@ -0,0 +1,19 @@
+package OopsConcept;
+
+public class RunTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.run();
+        dog.breeds();
+
+        Cow cow = new Cow();
+        cow.Eat();
+        cow.run();
+
+        Cat cat = new Cat();
+        cat.voice();
+
+
+
+    }
+}
